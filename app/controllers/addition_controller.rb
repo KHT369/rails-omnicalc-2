c6a252665c4ee665204915b806d1b65a56afcff6
KHT369 <ktsao@chicagobooth.edu>
2024-11-01 22:54:30 +0000

    save 1

--- app/controllers/addition_controller.rb
+++ app/controllers/addition_controller.rb
@@ -1,13 +1,13 @@
 class AdditionController < ApplicationController
   def show_addition_form
-    render({ :template => "addition_templates/add_form" })
+    render({ :template => "addition_templates/addition_form" })
   end
 
   def add_these
-    @first_number = params.fetch("first_number").to_f
-    @second_number = params.fetch("second_num").to_f
+    @first_numb = params.fetch("first_num").to_f
+    @second_numb = params.fetch("second_num").to_f
 
-    @result = @first_number + @second_number
+    @result = @first_numb + @second_numb
 
 
     render({ :template => "addition_template/add_results" })
