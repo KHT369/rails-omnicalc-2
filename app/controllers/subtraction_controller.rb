e15081ac434fd3ad33878d054047d34fc4312b21
KHT369 <ktsao@chicagobooth.edu>
2024-11-01 23:25:06 +0000

    save 3

--- app/controllers/subtraction_controller.rb
+++ app/controllers/subtraction_controller.rb
@@ -4,8 +4,8 @@ class SubtractionController < ApplicationController
   end
 
   def subtract_these
-    @first_numb = params.fetch("first_num").to_f
-    @second_numb = params.fetch("second_num").to_f
+    @first_numb = params.fetch("first_number").to_f
+    @second_numb = params.fetch("second_number").to_f
 
     @result = @second_numb - @first_numb 
 
